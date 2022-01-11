74c3b758e6ea4f34a9a4df0a9ee71e068d69748e
WANGHanshuo1220 <whs13720400638@163.com>
2022-01-11 17:58:56 +0100

    second commit for server-client: finished client.py (central naming server)

--- client.py
+++ client.py
@@ -3,15 +3,48 @@ import requests, json
 
 def send_message():
     """ TODO """
-    pass
+    print("Please enter the message")
+    PORT = "90"
+    SRVADR = "127.0.0.1"
+    SUBURL = "/message"
+    URL = "http://" + SRVADR + ":" + PORT + SUBURL
+    mesg = input()
+    data = mesg
+    r = requests.post(URL, data=json.dumps(data))
+    if r.status_code == 500:
+        print("Can't send this message")
+    else:
+        return r
 
 def add_user():
     """ TODO """
-    pass
+    print("Please enter the name and password in this form:<name> <password>")
+    PORT = "90"
+    SRVADR = "127.0.0.1"
+    SUBURL = "/add_user"
+    URL = "http://" + SRVADR + ":" + PORT + SUBURL
+    userinfo = input()
+    data = userinfo
+    r = requests.post(URL, data=json.dumps(data))
+    if r.status_code == 500:
+        print("Can't add this person in the database")
+    else:
+        return r
 
 def require_ip():
     """ TODO """
-    pass
+    print("Please enter the name for searching her/his IP adress")
+    name = input()
+    PORT = "90"
+    SRVADR = "127.0.0.1"
+    SUBURL = "/ip"
+    FUNC = '?name=' + name
+    URL = "http://" + SRVADR + ":" + PORT + SUBURL + FUNC
+    r =  requests.get(URL)
+    if r.status_code == 500:
+        print("Can't find this person in the database")
+    else:
+        return print(r.text)
     
 
 def disconnect():
@@ -19,5 +52,18 @@ def disconnect():
     pass
 
 if __name__ == '__main__':
-    pass
+
+    while True:
+        
+        print("Veuillez saisir la fonctionalite")
+        print("1.Add user 2.Send Message 3.Get IP")
+
+        cmd = input()
+
+        if cmd == '1':
+            add_user()
+        elif cmd == '2':
+            send_message()
+        elif cmd == '3':
+            require_ip()
             
