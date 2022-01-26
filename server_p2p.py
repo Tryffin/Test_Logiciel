01f89122a1b443f28b611c0fa59ef04f71776e0e
Kzel <baoyining12@gmail.com>
2022-01-26 22:45:30 +0100

    [US1_MVP1_P2P] Created and completed Class MyServer

--- server_p2p.py
+++ server_p2p.py
@@ -9,3 +9,36 @@ class Connector(object):
         self.password = password
         self.addrPort = addrPort
         self.conObj = conObj
+
+class MyServer(socketserver.BaseRequestHandler):
+
+    def handle(self):
+        print("got connection from",self.client_address)
+        register = False
+        while True:
+            conn = self.request
+            data = conn.recv(1024)
+            if not data:
+                continue
+            dataobj = json.loads(data.decode('utf-8'))
+
+            if type(dataobj) == list and not register:
+                account = dataobj[0]
+                password = dataobj[1]
+                conObj = Connector(account,password,self.client_address,self.request)
+                connLst.append(conObj)
+                register = True
+                continue
+            print(connLst)
+
+            if len(connLst) > 1 and type(dataobj) == dict:
+                sendok = False
+                for obj in connLst:
+                    if dataobj['to'] == obj.account:
+                        obj.conObj.sendall(data)
+                        sendok = True
+                if sendok == False:
+                    print('no target valid!')
+            else:
+                conn.sendall('nobody recevied!'.encode('utf-8'))
+                continue
